77f78643a10b8eb8e63766ba4c314b7b8cb73fa8
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-12 15:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function sayHiToGrandma(string)
   }
   else if(string === string.toLowerCase())
   {
-    return "I can't hear you"
+    return "I can\'t hear you"
   }
   else
   {
-    return "I love you, too!"
+    return "I love you, too."
   }
 }
